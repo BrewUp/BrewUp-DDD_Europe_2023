0834f33c2d98d7e258ac89c135b1f05e224002ae
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-05-04 08:23:32 +0200

    Injected IServiceBus

--- src/Purchases/Brewup.Purchases.ApplicationService/Concretes/PurchasesOrchestrator.cs
+++ src/Purchases/Brewup.Purchases.ApplicationService/Concretes/PurchasesOrchestrator.cs
@@ -2,26 +2,27 @@
 using Brewup.Purchases.ApplicationService.BindingModels;
 using Brewup.Purchases.Messages.Commands;
 using Brewup.Purchases.SharedKernel.DomainIds;
+using Muflone.Persistence;
 
 namespace Brewup.Purchases.ApplicationService.Concretes;
 
 public sealed class PurchasesOrchestrator : IPurchasesOrchestrator
 {
-	//private readonly IServiceBus _serviceBus;
+	private readonly IServiceBus _serviceBus;
 
-	//public PurchasesOrchestrator(IServiceBus serviceBus)
-	//{
-	//	_serviceBus = serviceBus ?? throw new ArgumentNullException(nameof(serviceBus));
-	//}
+	public PurchasesOrchestrator(IServiceBus serviceBus)
+	{
+		_serviceBus = serviceBus ?? throw new ArgumentNullException(nameof(serviceBus));
+	}
 
 	public async Task<string> CreateOrderAsync(Order order, CancellationToken cancellationToken)
 	{
 		//Application level validation
 		var createOrder = new CreateBuyOrder(new OrderId(order.Id),
 			new SupplierId(order.SupplierId), order.Date,
-			order.Lines.ToDto()); 
+			order.Lines.ToDto());
 
-		//await _serviceBus.SendAsync(createOrder, cancellationToken);
+		await _serviceBus.SendAsync(createOrder, cancellationToken);
 
 		return order.Id.ToString();
 	}
